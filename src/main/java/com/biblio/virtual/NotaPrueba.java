6f9448e0267c64d4f07a28d9dd337b64dba03ca4
FerNanclou <133546403+FerNanclou@users.noreply.github.com>
2025-09-18 14:58:07 -0500

    chore: agregar clase NotaPrueba para práctica de merge

---
+++ src/main/java/com/biblio/virtual/NotaPrueba.java
@@ -0,0 +1,11 @@
+package com.biblio.virtual;
+
+
+ /* Clase de prueba para demostrar la creación de ramas y fusión (merge).
+  No afecta la lógica del sistema. */
+ 
+public class NotaPrueba {
+    public static void mostrarMensaje() {
+        System.out.println("Este es un archivo de prueba para practicar Git merge.");
+    }
+}
